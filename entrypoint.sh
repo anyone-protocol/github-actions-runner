c03b9fc7c00b7c056051538a4ecc2c5a3817eadc
Jim Toth <jim@artby.city>
2025-08-18 18:57:13 -0400

    More logging to entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,17 +6,19 @@ echo "Generating JWT..."
 JWT=$(
   ./generate-github-app-jwt.sh ${CLIENT_ID} ${PRIVATE_KEY_PATH}
 )
-
+echo "Got JWT: ${#JWT}"
+echo "Installation ID: ${#INSTALLATION_ID}"
 echo "Requesting Installation Access Token..."
 ACCESS_TOKEN=$(
   curl -X POST \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $(cat jwt)" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    "https://api.github.com/app/installations/${INSTALLATION_ID}/access_tokens" \
+    https://api.github.com/app/installations/${INSTALLATION_ID}/access_tokens \
     | jq .token --raw-output
 )
-
+echo "Access Token: ${#ACCESS_TOKEN}"
+echo "Org: ${ORG}"
 echo "Requesting Runner Registration Token..."
 REG_TOKEN=$(
   curl -X POST \
@@ -26,7 +28,8 @@ REG_TOKEN=$(
     https://api.github.com/orgs/${ORG}/actions/runners/registration-token \
     | jq .token --raw-output
 )
-
+echo "Registration Token: ${#REG_TOKEN}"
+echo "Runner Name: ${RUNNER_NAME}"
 ./config.sh --url https://github.com/${ORG} --token ${REG_TOKEN} --name ${RUNNER_NAME}
 
 cleanup() {
