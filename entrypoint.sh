04454d46538083f7a7cb0f8b75c4c0ad2d3024d8
Jim Toth <jim@artby.city>
2025-08-18 18:45:54 -0400

    Adds logging to docker entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,22 @@
 
 # Modified from: https://baccini-al.medium.com/how-to-containerize-a-github-actions-self-hosted-runner-5994cc08b9fb
 
+echo "Generating JWT..."
 JWT=$(
   ./generate-github-app-jwt.sh ${CLIENT_ID} ${PRIVATE_KEY_PATH}
 )
 
+echo "Requesting Installation Access Token..."
 ACCESS_TOKEN=$(
   curl -X POST \
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $(cat jwt)" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    "https://api.github.com/app/installations/${INSTALLATION_ID}/access_tokens"
+    "https://api.github.com/app/installations/${INSTALLATION_ID}/access_tokens" \
     | jq .token --raw-output
 )
 
+echo "Requesting Runner Registration Token..."
 REG_TOKEN=$(
   curl -X POST \
     -H "Accept: application/vnd.github+json" \
